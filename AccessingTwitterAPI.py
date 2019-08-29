a4ed525eb280170f06b3747e9147a14919946b10
rgbk21 <rajgaurav.bk@gmail.com>
2019-08-29 11:59:10 -0500

    -Added code to print the user's details like followers_count, friends_count, and other stuff. Useful for validating users.

--- AccessingTwitterAPI.py
+++ AccessingTwitterAPI.py
@@ -11,7 +11,7 @@ from tweepy import Stream
 from tweepy import API
 from tweepy import Cursor
 from textblob import TextBlob
-import re #reges library
+import re  # reges library
 from credentials import consumer_key
 from credentials import consumer_secret
 from credentials import access_token
@@ -23,7 +23,7 @@ import json
 
 # # # # TWITTER CLIENT # # # #
 class TwitterClient():
-    #twitter_user=None is a default specification of the argument to the function. If no parameter is specified then the
+    # twitter_user=None is a default specification of the argument to the function. If no parameter is specified then the
     # code just returns the information of the self user.
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
@@ -33,7 +33,7 @@ class TwitterClient():
     def get_twitter_client_api(self):
         return self.twitter_client
 
-    #num_tweets is the number of tweets that you want to extract from the user's timeline
+    # num_tweets is the number of tweets that you want to extract from the user's timeline
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
         for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
@@ -42,7 +42,7 @@ class TwitterClient():
 
     def get_friend_list(self, num_friends):
         friend_list = []
-        #the id argument in the Cursor object helps us specify the user for which we want to pull the data
+        # the id argument in the Cursor object helps us specify the user for which we want to pull the data
         for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
             friend_list.append(friend)
         return friend_list
@@ -53,6 +53,7 @@ class TwitterClient():
             home_timeline_tweets.append(tweet)
         return home_timeline_tweets
 
+
 # # # # TWITTER AUTHENTICATER # # # #
 class TwitterAuthenticator():
 
@@ -61,16 +62,17 @@ class TwitterAuthenticator():
         auth.set_access_token(access_token, access_token_secret)
         return auth
 
+
 # # # # TWITTER STREAMER # # # #
 class TwitterStreamer():
     """
     Class for streaming and processing live tweets.
     """
-    def __init__(self):#constructor?
+
+    def __init__(self):  # constructor?
         self.twitter_authenticator = TwitterAuthenticator()
 
-
-    #hash_tag_list is the words that you will be using to filter the tweets
+    # hash_tag_list is the words that you will be using to filter the tweets
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         # This handles Twitter authentication and the connection to Twitter Streaming API
         listener = TwitterListener(fetched_tweets_filename)
@@ -80,11 +82,13 @@ class TwitterStreamer():
         # This line filter Twitter Streams to capture data by the keywords:
         stream.filter(track=hash_tag_list)
 
+
 # # # # TWITTER STREAM LISTENER # # # #
 class TwitterListener(StreamListener):
     """
     This is a basic listener that just prints received tweets to stdout.
     """
+
     def __init__(self, fetched_tweets_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
 
@@ -109,15 +113,17 @@ class TwitterListener(StreamListener):
 
     def on_error(self, status):
         if status == 420:
-            #Returning false on_data method in case the rate_limit is exceeded. This will send a 420 error from Twitter
+            # Returning false on_data method in case the rate_limit is exceeded. This will send a 420 error from Twitter
             print("Warning: rate_limit reached for Twitter")
-            return False #KILL STREAM
+            return False  # KILL STREAM
         print(status)
+
 
 class TweetAnalyzer():
     """
     Functionality for analyzing and categorizing content from tweets
     """
+
     def clean_tweet(self, tweet):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
@@ -130,13 +136,11 @@ class TweetAnalyzer():
         else:
             return -1
 
-
-
     def tweets_to_dataframe(self, tweets):
-        #tweet.text extracts the text from each tweet in tweets
+        # tweet.text extracts the text from each tweet in tweets
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=["tweets"])
-        #df["id"] creates a new column in the dataframe called "id"
-        #So this line of code: looks at the tweets object passed as the argument, cycles through each tweet in tweets, copies the id of the tweet, stores it in an array, converts it into a numpy array, adds it to the pd dataframe under the "id" column
+        # df["id"] creates a new column in the dataframe called "id"
+        # So this line of code: looks at the tweets object passed as the argument, cycles through each tweet in tweets, copies the id of the tweet, stores it in an array, converts it into a numpy array, adds it to the pd dataframe under the "id" column
         df["id"] = np.array([tweet.id for tweet in tweets])
         df["len"] = np.array([len(tweet.text) for tweet in tweets])
         df["creation"] = np.array([tweet.created_at for tweet in tweets])
@@ -152,25 +156,25 @@ if __name__ == '__main__':
     hash_tag_list = ['#HongKongAirport', '#HongKong', 'Hong Kong']
     count = 0
     fetched_tweets_filename = "tweets_HK_" + str(count) + "_.txt"
-    #Uncomment the below 2 lines for Streaming Tweets to the file
-    twitter_streamer = TwitterStreamer()#Creates an object of the class TwitterStreamer
-    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    # Uncomment the below 2 lines for Streaming Tweets to the file
+    # twitter_streamer = TwitterStreamer()#Creates an object of the class TwitterStreamer
+    # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
 
-    while True:
-        count = count + 1
-        fetched_tweets_filename = "tweets_HK_" + str(count) + "_.txt"
-        twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
-        # gauth = GoogleAuth()
-        # gauth.LocalWebserverAuth()
-        # drive = GoogleDrive(gauth)
+    # while True:
+    #     count = count + 1
+    #     fetched_tweets_filename = "tweets_HK_" + str(count) + "_.txt"
+    #     twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    # gauth = GoogleAuth()
+    # gauth.LocalWebserverAuth()
+    # drive = GoogleDrive(gauth)
 
-    #Uncomment the below lines for getting tweets of a specific user:
+    # Uncomment the below lines for getting tweets of a specific user:
     # twitter_client = TwitterClient("CppCon")
-    # twitter_client = TwitterClient()#creating an object from TwitterClient class
-    # api = twitter_client.get_twitter_client_api()
-    # tweet_analyzer = TweetAnalyzer()#Creating an object from TweetAnalyzer class
-    #api.user_timeline is a function that is provided by the twitter API. It is not a function that we have written.
-    #Check: http://docs.tweepy.org/en/v3.8.0/api.html for more examples of such functions
+    twitter_client = TwitterClient()  # creating an object from TwitterClient class
+    api = twitter_client.get_twitter_client_api()
+    tweet_analyzer = TweetAnalyzer()  # Creating an object from TweetAnalyzer class
+    # api.user_timeline is a function that is provided by the twitter API. It is not a function that we have written.
+    # Check: http://docs.tweepy.org/en/v3.8.0/api.html for more examples of such functions
 
     # This code will get the latest "count" number of tweets from the user "screen_name".
     # It will build a dataframe with the columns specified in the tweets_to_dataframe method
@@ -183,8 +187,129 @@ if __name__ == '__main__':
     #     print(tweet._json, sep="")
     # print("Completed Printing individual tweet objects from User")
 
+    # Use the following Code to get the user related details like follower_count, friend_count, user_description
+    test = api.lookup_users(user_ids=['1090715513586679813',
+                                      '25073877',
+                                      '60783724',
+                                      '4513039632',
+                                      '30188594',
+                                      '60113110',
+                                      '1639530362',
+                                      '3228637992',
+                                      '49616273',
+                                      '471516779',
+                                      '18956073',
+                                      '1093705708619681797',
+                                      '347927511',
+                                      '4828068038',
+                                      '18798768',
+                                      '809887021',
+                                      '757893380',
+                                      '32626129',
+                                      '494974123',
+                                      '1339835893',
+                                      '1877831',
+                                      '138141969',
+                                      '72345868',
+                                      '252751061',
+                                      '87775422',
+                                      '1115874631',
+                                      '592730371',
+                                      '76347918',
+                                      '112551613',
+                                      '303862998',
+                                      '872683897',
+                                      '19739126',
+                                      '109579590',
+                                      '896778691954434048',
+                                      '15745368',
+                                      '774375759284756480',
+                                      '1105235739355291649',
+                                      '173162992',
+                                      '1117012713329152000',
+                                      '45808079',
+                                      '2466036350',
+                                      '62093225',
+                                      '1077127237999063040',
+                                      '1186149476',
+                                      '951302891708583936',
+                                      '1053329245919940608',
+                                      '21366823',
+                                      '292929271',
+                                      '1080894931311431682',
+                                      '16288136',
+                                      '23922797',
+                                      '1330457336',
+                                      '27284203',
+                                      '21439144',
+                                      '2836421',
+                                      '55734987',
+                                      '1073786153910853632',
+                                      '30354991',
+                                      '471677441',
+                                      '50943008',
+                                      '748365116769452032',
+                                      '27000730',
+                                      '1079776144524754944',
+                                      '7493052',
+                                      '4914384040',
+                                      '1095500352651173888',
+                                      '41634520',
+                                      '231712188',
+                                      '930552552302792705',
+                                      '1144711684013076483',
+                                      '759251',
+                                      '807095',
+                                      '632962034',
+                                      '3071162052',
+                                      '103280363',
+                                      '3384104129',
+                                      '563016943',
+                                      '38495835',
+                                      '65172298',
+                                      '379620203',
+                                      '525658038',
+                                      '1001165088697585664',
+                                      '15764644',
+                                      '34713362',
+                                      '409062401',
+                                      '17494010',
+                                      '28785486',
+                                      '380648579',
+                                      '256360738',
+                                      '948464246064427008',
+                                      '568400331',
+                                      '893680236',
+                                      '6440792',
+                                      '820354372827758592',
+                                      '338985020',
+                                      '928444328',
+                                      '240107748',
+                                      '1048214603077808128',
+                                      '873115441303924736'
+                                      ])
+    df = pd.DataFrame(data=test)
+    df["screen_name"] = np.array([user.screen_name for user in test])
+    df["user.id_str"] = np.array([user.id_str for user in test])
+    df["user.followers_count"] = np.array([user.followers_count for user in test])
+    df["user.friends_count"] = np.array([user.friends_count for user in test])
+    df["user.verified"] = np.array([user.verified for user in test])
+    df["user.description"] = np.array([user.description for user in test])
+    # print(df[["user.description"]].to_string())
+    print(df[["screen_name", "user.id_str", "user.followers_count", "user.friends_count", "user.verified"]].to_string())
+    index = 0
+    for user in test:
+        #     print("user.screen_name: " + user.screen_name)
+        #     print("user.id_str: " + user.id_str)
+        print("index: "+ str(index) + ": " + user.description)
+        index += 1
+    #     print("followers_count: " + str(user.followers_count))
+    #     print("friends_count: " + str(user.friends_count))
+    #     print("user.verified: " + str(user.verified))
+    #     print("************************************************")
+
     # df = tweet_analyzer.tweets_to_dataframe(tweets)
-    #Adding the sentiment to the dataframe
+    # Adding the sentiment to the dataframe
     # df["sentiment"] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df["tweets"]])
 
     # print(dir(tweets[0]))#prints outs the attributes that we can use for each tweet
@@ -192,5 +317,5 @@ if __name__ == '__main__':
     # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
     #     print(df)
 
-    #print(tweets)
+    # print(tweets)
     # print(twitter_client.get_friend_list(10))
